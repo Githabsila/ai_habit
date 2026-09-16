640b7586a7cded6b3ea7e0a16007aa64dfd427f1
Githabsila <egormatancev12@gmail.com>
2026-09-16 21:33:35 +0300

    fix habit intent detection

--- webapp/static/app.js
+++ webapp/static/app.js
@@ -210,7 +210,7 @@
     }, { passive: true });
   })();
 
-  const RING_CIRCUMFERENCE = 314.159265; // 2 * PI * 50
+  const RING_CIRCUMFERENCE = 2 * Math.PI * 50; // exact circumference for r=50
 
   function pluralRu(n, one, few, many) {
     n = Math.abs(Number(n) || 0);
